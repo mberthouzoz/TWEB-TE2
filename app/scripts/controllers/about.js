654bf34fc498e6fce734211ec4fa1ce04d7cdaa8
Berthouzoz Michaël <hello@michael-berthouzoz.me>
2015-12-30 12:00:05 +0100

    yo angular first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name anguSeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the anguSeApp
+ */
+angular.module('anguSeApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
